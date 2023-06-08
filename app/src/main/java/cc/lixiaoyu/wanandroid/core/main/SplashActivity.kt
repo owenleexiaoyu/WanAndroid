db3fe8ff7b0dd2e0a5f59bd2d492eee235a3ff1d
OwenLee <2924354040@qq.com>
2023-06-08 23:29:18 +0800

    Opt: integrate CodeLoactor SDK and opt SplashActivity

--- app/src/main/java/cc/lixiaoyu/wanandroid/core/main/SplashActivity.kt
+++ app/src/main/java/cc/lixiaoyu/wanandroid/core/main/SplashActivity.kt
@@ -1,12 +1,13 @@
-package cc.lixiaoyu.wanandroid.ui.activity
+package cc.lixiaoyu.wanandroid.core.main
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import cc.lixiaoyu.wanandroid.R
-import cc.lixiaoyu.wanandroid.core.main.MainActivity
 import kotlinx.coroutines.*
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +16,7 @@ class SplashActivity: AppCompatActivity() {
         GlobalScope.launch(Dispatchers.Main) {
             delay(2000)
             startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finish()
         }
     }
 }
